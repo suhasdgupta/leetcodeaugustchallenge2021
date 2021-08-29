cf3aa57e3ae436d159ee4e4860342e0b35639e00
Suhaas D <suhasdgupta55555@gmail.com>
2021-08-29 20:15:47 +0530

    Create PatchingArray.java

---
+++ PatchingArray.java
@@ -0,0 +1,16 @@
+class PatchingArray {
+    public int minPatches(int[] nums, int n) {
+        long missing = 1;
+        int patches = 0, i = 0;
+
+        while (missing <= n) {
+            if (i < nums.length && nums[i] <= missing) {
+                missing += nums[i++];
+            } else {
+                missing += missing;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
